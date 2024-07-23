cd9858a3395aec51bf8ea2212accfe07bef3a8b5
Giovani de SOUZA <giovanidesouza999@gmail.com>
2024-07-23 03:17:47 +0100

    feat: Update UI of sidebar

--- src/Header/NavBar.jsx
+++ src/Header/NavBar.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
-import '../styles/header/NavBar.css'
+import React from "react";
+import "../styles/header/NavBar.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { RxCross2 } from "react-icons/rx";
 const NavBar = () => {
   return (
-    <nav className='navbar'>
-      <div className="hamburger"><GiHamburgerMenu />
-      <RxCross2 /></div>
-      <ul className='navbar-list'>
-       <a><li className='list-item'>Documentation</li></a> 
-       <a><li className='list-item'>Telegram</li></a> 
-       <a><li className='list-item'>Accéder à CASHGEN</li></a> 
+    <nav className="navbar">
+      <div className="hamburger">
+        <GiHamburgerMenu />
+        <RxCross2 />
+      </div>
+      <ul className="navbar-list">
+        <li
+          style={{ borderBottom: "3px solid #1289f8", color: "#1289f8" }}
+          className="list-item"
+        >
+          Documentation
+        </li>
+        <li className="list-item">Telegram</li>
+        <li className="list-item">Accéder à CASHGEN</li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
